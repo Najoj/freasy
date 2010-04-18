a6298d87a841cb92a5ab620114a60de60db400dc
ohassel <ohassel@497c142e-ff2b-8ecd-3c6c-fae733d823d1>
2010-04-18 15:22:10 +0000

    Har ändrat om till vad som (förhoppningsvis) ska vara korrekta XML-radbyten istället för "\n".

--- server/xmlParser/ExceptionMessages.java
+++ server/xmlParser/ExceptionMessages.java
@@ -15,32 +15,39 @@ public class ExceptionMessages
 
 	public static String XMLNotAttributeExceptionMessage( String superTag, String attribute, String extracted )
 	{
-		return "The attribute '"+ extracted+"' inside tag '"+attribute+"' at tag '" + superTag+"' is not a legal attribute according to spec."+"\n\n"+
+		return "The attribute '"+ extracted+"' inside tag '"+attribute+"' at tag '" + superTag+"' is not a legal attribute according to spec."+ 
+		ParserConstants.getLineBreak()+ParserConstants.getLineBreak()+
 		  "Must be one of the following: "+ParserConstants.listAllowedAttributes();
 	}
 	
 	public static String XMLNotLegalOperatorExceptionMessage( String superTag, String attribute, String extracted )
 	{
-		return "The value "+extracted+" in attribute '"+attribute+"' in tag '"+superTag+"' is not an legal operator"+"\n\n"+
+		return "The value "+extracted+" in attribute '"+attribute+"' in tag '"+superTag+"' is not an legal operator"+
+		ParserConstants.getLineBreak()+ParserConstants.getLineBreak()+
 		  "Must be an operator of one of the following types.";
 	}
 	
 	public static String XMLNotLegalIntegerExceptionMessage( String superTag, String attribute, String extracted )
 	{
-		return "The value "+extracted+" in attribute '"+attribute+"' in tag '"+superTag+"' is not an integer"+"\n\n"+
+		return "The value "+extracted+" in attribute '"+attribute+"' in tag '"+superTag+"' is not an integer"+
+		ParserConstants.getLineBreak()+ParserConstants.getLineBreak()+
 		  "Must be an integer.";
 	}
 	
 	public static String XMLNotLegalMatchesValueExceptionMessage( String superTag, String attribute, String extracted )
 	{
-		return "The value "+extracted+" in attribute '"+attribute+"' in tag '"+superTag+"' is not a legal matchby field."+"\n\n"+
+		return "The value "+extracted+" in attribute '"+attribute+"' in tag '"+superTag+"' is not a legal matchby field."+
+		ParserConstants.getLineBreak()+ParserConstants.getLineBreak()+
 		  "Must be an text of one of the following standards."+ParserConstants.listAllowedMatchByFilters();
 	}
 	
 	public static String XMLNotLegalASC_DESCExceptionMessage( String superTag, String attribute, String extracted )
 	{
-		return "The value "+extracted+" in attribute '"+attribute+"' in tag '"+superTag+"' is not a legal 'direction' field."+"\n\n"+
-		  "Must be an 'ASC' or 'DESC'\n The attribute is not required, if not included request defaults to 'DESC'";
+		return "The value "+extracted+" in attribute '"+attribute+"' in tag '"+superTag+"' is not a legal 'direction' field."+
+		ParserConstants.getLineBreak()+ParserConstants.getLineBreak()+
+		  "Must be an 'ASC' or 'DESC'"+
+		  ParserConstants.getLineBreak()+ParserConstants.getLineBreak()+
+		  "The attribute is not required, if not included request defaults to 'DESC'";
 	}
 	
 	public static String XMLTagNotFoundExceptionMessage( String superTag)
@@ -50,8 +57,10 @@ public class ExceptionMessages
 	
 	public static String XMLParseNoPADAttributesFoundExceptionMessage( String superTag)
 	{
-		return "No legal PAD attributes found at tag '"+superTag+"'"+"\n\n"+
-		".Must include atleast one legal attribute."+"\n\n"+
+		return "No legal PAD attributes found at tag '"+superTag+"'"+
+		ParserConstants.getLineBreak()+ParserConstants.getLineBreak()+
+		".Must include atleast one legal attribute."+
+		ParserConstants.getLineBreak()+ParserConstants.getLineBreak()+
 		"Legal attributes are: "+ParserConstants.listAllowedAttributes();
 	}
 	
