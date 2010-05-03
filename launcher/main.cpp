a23a139783ddeda8b2fc1576cb227a15347fbea3
Christoffer.Janson <Christoffer.Janson@497c142e-ff2b-8ecd-3c6c-fae733d823d1>
2010-05-03 16:20:24 +0000

    Added some memory handling

--- launcher/main.cpp
+++ launcher/main.cpp
@@ -48,12 +48,8 @@ public:
 		if ((h / 2.5 - EXTENT_Y(logoExtents) / 2) > 0) {
 			maDrawImage(LOG, (w >> 1) - (EXTENT_X(logoExtents) >> 1), h / 2.5
 					- EXTENT_Y(logoExtents) / 2);
-			//prog =1.4;
-			//drawProgressbar(25, 100, 1.4);
 		} else {
 			maDrawImage(LOG, (w >> 1) - (EXTENT_X(logoExtents) >> 1), 0);
-			//prog =1.2;
-			//drawProgressbar(25, 100, 1.2);
 		}
 		maWait(5000);
 		state = Idle;
@@ -73,11 +69,13 @@ public:
 	}
 
 	void keyPressEvent(int keyCode, int nativeCode) {
-		// todo: handle key presses
-		if (download.isDownloading()) {
-			download.cancelDownloading();
+		// handle key presses
+		if (keyCode == MAK_0) {
+			if (download.isDownloading()) {
+				download.cancelDownloading();
+			}
+			maExit(0);
 		}
-		maExit(0);
 	}
 
 	void keyReleaseEvent(int keyCode, int nativeCode) {
@@ -85,7 +83,7 @@ public:
 	}
 
 	/*
-	 * Returns the date stored in browserVersion.sav as a String
+	 * Returns the data stored in browserVersion.sav as a String
 	 */
 	String getVersion() {
 		MAHandle store = maOpenStore(browserVSave, 0);
@@ -95,14 +93,14 @@ public:
 			maReadStore(store, data);
 			int size = maGetDataSize(data);
 			if (size > 0) {
+				//read current version of the browser on device to tmp
 				tmp.resize(size);
 				maReadData(data, tmp.pointer(), 0, size);
-				//printf("current version \n%s\n", tmp.c_str());
 				PlaceholderPool::put(data);
 				return tmp;
 			}
 		}
-		//printf("No previous version stored\n");//pre graphics
+		//No previous version stored
 		return "";
 	}
 
@@ -140,7 +138,6 @@ public:
 	 */
 	void downloadBrowser() {
 		state = DownBrowser;
-		//printf("Downloading the browser\n"); //pre graphics
 		maDestroyObject(browser);// destroy the old browser
 		download.beginDownloading(browserURL, 0);// begin downloading the browser
 	}
@@ -175,44 +172,47 @@ public:
 	}
 
 	void finishedDownloading(Downloader* downloader, MAHandle downloadedData) {
-		//printf("finished download\n");
 		if (state == DownBrowser) {
 			int size = maGetDataSize(downloadedData);
-			//printf("Size %i Bytes\n", size);
 			//store browser program in browser.sav
 			MAHandle store = maOpenStore(browserSave, MAS_CREATE_IF_NECESSARY);
-			maWriteStore(store, downloadedData);
-			maReadStore(store, browser);
-			maCloseStore(store, 0);
-			// store last-modified header in browserVersion.sav
-			MAHandle data = PlaceholderPool::alloc();
-			maCreateData(data, browserVersion.length());
-			maWriteData(data, browserVersion.c_str(), 0,
-					browserVersion.length());
-			MAHandle vStore =
-					maOpenStore(browserVSave, MAS_CREATE_IF_NECESSARY);
-			maWriteStore(vStore, data);
-			maCloseStore(vStore, 0);
-			PlaceholderPool::put(data);//remove data from memory
-			// load program
-			maWait(7000);//debug purpose
-			http.close();//close http connection to save money
-			maLoadProgram(browser, 1);
+			int res = maWriteStore(store, downloadedData);
+			if (res != STERR_FULL) {
+				maReadStore(store, browser);
+				maCloseStore(store, 0);
+				// store last-modified header in browserVersion.sav
+				MAHandle data = PlaceholderPool::alloc();
+				maCreateData(data, browserVersion.length());
+				maWriteData(data, browserVersion.c_str(), 0,
+						browserVersion.length());
+				MAHandle vStore = maOpenStore(browserVSave,
+						MAS_CREATE_IF_NECESSARY);
+				maWriteStore(vStore, data);
+				maCloseStore(vStore, 0);
+				PlaceholderPool::put(data);//remove data from memory
+				// load program
+				maWait(7000);//debug purpose
+				http.close();//close http connection to save money
+				maLoadProgram(browser, 1);
+			} else {
+				printf("Out of system memory. Press 0 to exit.");
+			}
 		} else if (state == DownProgram) {
 			MAHandle program = PlaceholderPool::alloc();
 			int size = maGetDataSize(downloadedData);
-			//printf("Size %i Bytes\n", size);
 			MAHandle store = maOpenStore("tmp.sav", MAS_CREATE_IF_NECESSARY);
-			maWriteStore(store, downloadedData);
-			maReadStore(store, program);
-			maCloseStore(store, -1);
-			//maWait(7000);//for debug
-			maLoadProgram(program, 1);
+			int res = maWriteStore(store, downloadedData);
+			if (res != STERR_FULL) {
+				maReadStore(store, program);
+				maCloseStore(store, -1);
+				maLoadProgram(program, 1);
+			} else {
+				printf("Out of system memory. Press 0 to exit.");
+			}
 		}
 	}
 
 	void notifyProgress(Downloader* down, int downloadedBytes, int totalBytes) {
-		//printf("progress %i\%\n", 100 * downloadedBytes / totalBytes);
 		drawProgressbar(downloadedBytes, totalBytes, 1.2);
 	}
 
@@ -224,11 +224,10 @@ public:
 	void error(Downloader* down, int errorCode) {
 		//printf("error %i\n", errorCode);
 		if (state == DownProgram) {
-			//printf("invalid Adress\n");
 			urlIterator++;
 			if (urlIterator > 3) {
-				//printf(
-				//		"There are no valid adresses to download this program from\n");
+				printf("There are no valid addresses to download ");
+				printf("this program from\n");
 				//todo: vad ska jag göra om det inte finns några adresser att ladda ner programmet från?
 			} else {
 				//printf("Downloading from %s\n", urls[urlIterator].c_str());
@@ -245,7 +244,6 @@ public:
 			maExit(0);
 		}
 		if (getVersion() == browserVersion) {
-			//printf("browser is up to date");
 			//load saved browser
 			MAHandle store = maOpenStore(browserSave, 0);
 			if (store > 0) {
@@ -258,11 +256,9 @@ public:
 					//todo: delete old files to remove error?
 					maExit(0);
 				}
-				//maWait(5000);//debug purpouse
 				maLoadProgram(browser, 1);
 			}
 		} else {
-			//printf("Download new browser");
 			downloadBrowser();
 		}
 		return;
@@ -271,6 +267,7 @@ public:
 	virtual void connReadFinished(Connection* c, int res) {
 		printf("connread\n");
 		printf("The program can't go here\n");
+		printf("or should not go here is more accurate\n");
 		return;
 	}
 
